9cadca0072f082ad50e253e655e586ae16a703c8
Catalina Arriagada <catalina.arriagada1@gmail.com>
2023-05-30 23:21:11 -0400

    styles & buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Logo from './images/logo.png';
+import Button from './Components/Button'
 
 function App() {
   return (
@@ -8,10 +9,30 @@ function App() {
         <img className='logo' src={Logo} alt='Logo' />
       </div>
       <div className="calculator-container">
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className="row">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className="row">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className="row">
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
         <div className="row"></div>
       </div>
     </div>
--- calculator/src/Components/Button.js
+++ calculator/src/Components/Button.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import '../styles/Button.css'
 
 function Button(props) {
+
+  const isOperator = valor => {
+    //we dont want this signs recognize like operator
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
     return (
-        <div>
+        <div className={`button-container ${isOperator(props.children) ? 'operator' : null}`.trimEnd()}>
           {props.children}
         </div>
     )
